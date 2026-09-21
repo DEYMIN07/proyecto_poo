b8c9f0cee9677616684428bf4d206a05a0f46626
Deymi <n00443762@upn.pe>
2026-09-21 11:40:32 -0500

    Implementación de sistema básico de registro de estudiante en Java_DEYMIN YOPLA

---
+++ proyecto_git/src/proyecto_git/Proyecto_git.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_git;
+
+/**
+ *
+ * @author deymi
+ */
+public class Proyecto_git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String nombre = "Deymin";
+        int edad = 20;
+        String carrera = "Ingeniería de Sistemas";
+
+        System.out.println("===== SISTEMA DE ESTUDIANTE =====");
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Carrera: " + carrera);
+
+        System.out.println("===============================");
+        System.out.println("Registro realizado correctamente.");
+        
+    }
+    
+}
