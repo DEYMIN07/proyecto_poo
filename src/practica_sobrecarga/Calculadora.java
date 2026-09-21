4671590fb41071af48c5b9e94ceb17b843516173
Deymi <n00443762@upn.pe>
2026-09-21 12:17:56 -0500

    commit_deymin

---
+++ src/practica_sobrecarga/Calculadora.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica_sobrecarga;
+
+/**
+ *
+ * @author deymi
+ */
+class Calculadora {
+    // Suma de dos números enteros
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    // Suma de tres números enteros
+    public int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Suma de dos números decimales
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+    //sobre carga (...)
+    
+}
